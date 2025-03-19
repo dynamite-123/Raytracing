49b44bab9246339e7d25321165201ecb0ff67ecf
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2025-03-19 13:05:49 +0530

    added rays

--- main.c
+++ main.c
@@ -38,12 +38,33 @@ void generate_rays(struct Circle circle, struct Ray rays[RAYS_NUMBER]) {
     for (int i=0; i<RAYS_NUMBER; i++) {
         double angle = ((double) i / RAYS_NUMBER) * 2 * M_PI;
         struct Ray ray = {circle.x, circle.y, angle};
-        printf("angle: %f\n", angle);
+        rays[i] = ray;
     }
 }
 
-void FillRays(SDL_Surface *surface, struct Ray rays[RAYS_NUMBER], Uint32) {
-    
+void FillRays(SDL_Surface *surface, struct Ray rays[RAYS_NUMBER], Uint32 color) {
+    for (int i=0; i<RAYS_NUMBER; i++){
+        struct Ray ray = rays[i];
+        int end_of_screen = 0, object_hit = 0;
+
+        double step = 1;
+        double x_draw = ray.x_start, y_draw = ray.y_start;
+
+        while (end_of_screen != 1 && object_hit != 1) {
+            x_draw += step * cos(ray.angle);
+            y_draw += step * sin(ray.angle);
+
+            SDL_Rect pixel = (SDL_Rect) {x_draw, y_draw, 1, 1};
+            SDL_FillRect(surface, &pixel, color);
+
+            if (x_draw < 0 || x_draw > WIDTH) {
+                end_of_screen = 1;
+            }
+            if (y_draw < 0 || y_draw > HEIGHT) {
+                end_of_screen = 1;
+            }
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
